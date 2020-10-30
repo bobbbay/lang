346a87e5503ddbbd9078c4ba534fb680de8037e6
Bobbbay <49123594+Bobbbay@users.noreply.github.com>
2020-10-30 00:08:16 -0400

    I understand bytecode & stacks now!

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main() {
     initVM();
     Chunk chunk;
     initChunk(&chunk);
+
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
@@ -24,6 +25,41 @@ int main() {
 
     writeChunk(&chunk, OP_DIVIDE, 123);
     writeChunk(&chunk, OP_NEGATE, 123);
+    /*
+    // 1 + 2 * 3 - 4 / -5
+
+    int constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+
+    constant = addConstant(&chunk, 4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 0);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 5);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_SUBTRACT, 123);
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_SUBTRACT, 123);
+    writeChunk(&chunk, OP_ADD, 123);
+    */
+
 
     writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
