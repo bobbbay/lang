06f6d0dccbaad101d6dfc1fa927ba9b7558dfebc
Bobbbay <49123594+Bobbbay@users.noreply.github.com>
2020-10-29 23:16:39 -0400

    Page 1 complete

--- chunk.h
+++ chunk.h
@@ -13,11 +13,12 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
     ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 void freeChunk(Chunk* chunk);
 
